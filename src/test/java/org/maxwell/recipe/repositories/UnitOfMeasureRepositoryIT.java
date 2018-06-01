2b6ac56eb7dff8f7a12df4172339592427bbb8c5
Sapna Deraje Radhakrishna <sapna.dr@gmail.com>
2018-06-01 12:02:04 -0700

    adding IT

---
+++ src/test/java/org/maxwell/recipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,38 @@
+package org.maxwell.recipe.repositories;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.maxwell.recipe.domain.UnitOfMeasure;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+	@Autowired
+	UnitOfMeasureRepository unitOfMeasureRepository;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFindByDescription() throws Exception {
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+		assertEquals("Teaspoon", uomOptional.get().getDescription());
+	}
+	
+	@Test
+	public void testFindByDescriptionCup() throws Exception {
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+		assertEquals("Cup", uomOptional.get().getDescription());
+	}
+
+}
