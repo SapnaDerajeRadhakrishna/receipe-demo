8f148f51dfcfa08f424e2e13bcdfd71d1be9928b
Sapna Deraje Radhakrishna <sapna.dr@gmail.com>
2018-06-11 17:30:19 -0700

    cobertura code coverage

--- src/test/java/org/maxwell/recipe/RecipeApplicationTest.java
+++ src/test/java/org/maxwell/recipe/RecipeApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RecipeApplicationTests {
+public class RecipeApplicationTest {
 
 	@Test
 	public void contextLoads() {
